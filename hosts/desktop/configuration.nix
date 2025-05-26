ceed1450cb0c257f3058503f1f5cbe1ce39ff8fb
2leander2 <leanderkroth@gmail.com>
2025-05-27 00:20:04 +0200

    Update configuration.nix

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -16,7 +16,7 @@ in
             "https://hyprland.cachix.org"
         ];
         trusted-public-keys = [
-            "cache.nixos.org-1:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
             "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
         ];
     };

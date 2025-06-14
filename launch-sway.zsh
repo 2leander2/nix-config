071be56f3b538663c61a22c4f2c3e1e486ce8d43
2leander2 <leanderkroth@gmail.com>
2025-06-14 17:48:59 +0200

    Add env variables for native wayland use in steam, firefox etc

--- launch-sway.zsh
+++ launch-sway.zsh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 # TODO: put these scripts somewhere else for modularity
 export SDL_VIDEODRIVER=wayland
+export NIXOS_OZONE_WL=1
+export MOZ_ENABLE_WAYLAND=1
+export WLR_NO_HARDWARE_CURSORS=1
 export _JAVA_AWT_WM_NONREPARENTING=1
 export QT_QPA_PLATFORM=wayland
 export XDG_CURRENT_DESKTOP=sway
